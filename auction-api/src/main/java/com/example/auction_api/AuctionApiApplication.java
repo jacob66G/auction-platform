9ac07aa1b80e7a641506d3ce16671d1e770a4a84
JAKUB GERWEL <kuba.gerwel@gmail.com>
2025-05-10 18:48:53 +0200

    init spring project

---
+++ auction-api/src/main/java/com/example/auction_api/AuctionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.auction_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApiApplication.class, args);
+	}
+
+}
